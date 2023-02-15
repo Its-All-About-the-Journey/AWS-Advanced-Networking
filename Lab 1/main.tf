407d27f17df2f7631dc2b25435663ee769fdb16b
Charles Uneze <charlesniklaus@gmail.com>
2023-02-15 15:31:30 +0100

    updated main.tf
    I removed the default security group resource, as I realized that adding this explicitly removes the default security group rule. Hence, pings or any other type of traffic will fail.

--- Lab 1/main.tf
+++ Lab 1/main.tf
@@ -7,105 +7,71 @@ provider "aws" {
   default_tags {
     tags = {
       Enviroment  = terraform.workspace
-      Provisioned = "Terraform"
     }
   }
 }
 
-resource "aws_vpc" "Lab-1-VPC" {
-  cidr_block       = var.aws_vpc_cidr
-  instance_tenancy = "default"
+resource "aws_vpc" "Lab_1_VPC" {
+  cidr_block       = var.aws_vpc_attributes.cidr_block
+  instance_tenancy = var.aws_vpc_attributes.instance_tenancy
 
   tags = {
-    name           = "Lab-1-VPC"
-    generated_from = "Terraform"
+    name           = "Lab_1_VPC"
   }
 }
 
-resource "aws_subnet" "Lab-1-Subnet" {
-  vpc_id                  = aws_vpc.Lab-1-VPC.id
+resource "aws_subnet" "Lab_1_Subnet" {
+  vpc_id                  = aws_vpc.Lab_1_VPC.id
   cidr_block              = var.aws_subnet_attributes.cidr_block
   map_public_ip_on_launch = "true"
   availability_zone       = var.aws_subnet_attributes.availability_zone
 
   tags = {
-    name           = "Lab-1-Subnet"
-    generated_from = "Terraform"
+    name           = "Lab_1_Subnet"
   }
 }
 
-resource "aws_internet_gateway" "Lab-1-IGW" {
-  vpc_id = aws_vpc.Lab-1-VPC.id
+resource "aws_internet_gateway" "Lab_1_IGW" {
+  vpc_id = aws_vpc.Lab_1_VPC.id
 
   tags = {
-    name           = "Lab-1-IGW"
-    generated_from = "Terraform"
+    name           = "Lab_1_IGW"
   }
 }
 
-resource "aws_default_route_table" "Lab-1-RT" {
-  default_route_table_id =  aws_vpc.Lab-1-VPC.default_route_table_id
+resource "aws_default_route_table" "Lab_1_RT" {
+  default_route_table_id =  aws_vpc.Lab_1_VPC.default_route_table_id
 
   route {
-    cidr_block = var.Internet_IP_for_Routes
-    gateway_id = aws_internet_gateway.Lab-1-IGW.id
+    cidr_block = var.Internet_IP_for_Route_a_string
+    gateway_id = aws_internet_gateway.Lab_1_IGW.id
   }
 
   tags = {
-    name           = "Lab-1-RT"
-    generated_from = "Terraform"
+    name           = "Lab_1_RT"
   }
 }
 
-resource "aws_default_security_group" "Lab-1-SG" {
-  vpc_id      = aws_vpc.Lab-1-VPC.id
-
-  tags = {
-    name           = "Lab-1-SG"
-    generated_from = "Terraform"
-  }
-}
-
-resource "aws_security_group_rule" "Allow_Internet_Incoming" {
-  type              = "ingress"
-  protocol          = "tcp"
-  from_port         = var.Allow_Internet.from_port
-  to_port           = var.Allow_Internet.to_port
-  cidr_blocks       = var.Internet_IP_for_SG
-  security_group_id = aws_default_security_group.Lab-1-SG.id
-}
-
-resource "aws_security_group_rule" "Allow_Internet_Outgoing" {
-  type              = "egress"
-  protocol          = "tcp"
-  from_port         = var.Allow_Internet.from_port
-  to_port           = var.Allow_Internet.to_port
-  cidr_blocks       = var.Internet_IP_for_SG
-  security_group_id = aws_default_security_group.Lab-1-SG.id
-}
-
-resource "aws_eip" "Lab-1-EIP" {
-  instance = aws_instance.AWS-Linux.id
+resource "aws_eip" "Lab_1_EIP" {
+  instance = aws_instance.AWS_Linux.id
   vpc      = true
   tags = {
-    name           = "Lab-1-EIP"
-    generated_from = "Terraform"
+    name           = "Lab_1_EIP"
   }
 }
 
-resource "aws_instance" "AWS-Linux" {
-  ami                    = var.aws_instance.ami
-  instance_type          = var.aws_instance.instance_type
-  tenancy                = "default"
-  availability_zone      = var.aws_instance.availability_zone
+resource "aws_instance" "AWS_Linux" {
+  ami                    = var.aws_instance_AWS_Linux_attributes.ami
+  instance_type          = var.aws_instance_AWS_Linux_attributes.instance_type
+  tenancy                = var.aws_instance_AWS_Linux_attributes.tenancy
+  availability_zone      = var.aws_instance_AWS_Linux_attributes.availability_zone
 
   tags = {
-    name           = "Lab-1-AWS-Linux"
-    generated_from = "Terraform"
+    name           = "Lab_1_AWS_Linux"
   }
 }
 
-resource "aws_eip_association" "Lab-1-EIP-Association" {
-  instance_id   = aws_instance.AWS-Linux.id
-  allocation_id = aws_eip.Lab-1-EIP.id
+resource "aws_eip_association" "Lab_1_EIP-Association" {
+  instance_id   = aws_instance.AWS_Linux.id
+  allocation_id = aws_eip.Lab_1_EIP.id
 }
