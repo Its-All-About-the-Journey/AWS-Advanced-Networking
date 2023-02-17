0ee13a1238a5436d38512f03eeffc9657c3cc086
Charles Uneze <charlesniklaus@gmail.com>
2023-02-17 11:37:56 +0100

    Update main.tf

--- Lab 1/main.tf
+++ Lab 1/main.tf
@@ -4,11 +4,6 @@ provider "aws" {
   #shared_credentials_files = "C:\\Users\\Admin\\.aws\\credentials"
   access_key = var.access_key
   secret_key = var.secret_key
-  default_tags {
-    tags = {
-      Enviroment  = terraform.workspace
-    }
-  }
 }
 
 resource "aws_vpc" "Lab_1_VPC" {
