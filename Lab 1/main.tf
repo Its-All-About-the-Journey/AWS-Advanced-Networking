abd5eaa126c2f89fa8856b5a27b0c3f2e977c5f5
Charles Uneze <charlesniklaus@gmail.com>
2023-02-26 17:43:30 +0100

    Made the code more readable

--- Lab 1/main.tf
+++ Lab 1/main.tf
@@ -1,72 +1,129 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = var.region
+  region = "eu-west-2"
   #shared_credentials_files = "C:\\Users\\Admin\\.aws\\credentials"
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
-resource "aws_vpc" "Lab_1_VPC" {
-  cidr_block       = var.aws_vpc_attributes.cidr_block
-  instance_tenancy = var.aws_vpc_attributes.instance_tenancy
+# Create VPC
+resource "aws_vpc" "VPC_A" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
   tags = {
-    name           = "Lab_1_VPC"
+    name = "VPC_A"
   }
 }
 
-resource "aws_subnet" "Lab_1_Subnet" {
-  vpc_id                  = aws_vpc.Lab_1_VPC.id
-  cidr_block              = var.aws_subnet_attributes
-  map_public_ip_on_launch = "true"
-  availability_zone       = var.availability_zone
+# Create Subnet
+resource "aws_subnet" "VPC_A_Subnet_A" {
+  vpc_id                  = aws_vpc.VPC_A.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "eu-west-2a"
 
   tags = {
-    name           = "Lab_1_Subnet"
+    name = "VPC_A_Subnet_A"
   }
 }
 
-resource "aws_internet_gateway" "Lab_1_IGW" {
-  vpc_id = aws_vpc.Lab_1_VPC.id
+# Specify the default NACL, regardless if the VPC creates it on default
+resource "aws_default_network_acl" "Default_VPC_A_NACL" {
+  default_network_acl_id = aws_vpc.VPC_A.default_network_acl_id
+
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
 
   tags = {
-    name           = "Lab_1_IGW"
+    "name" = "Default_VPC_A_NACL"
   }
 }
 
-resource "aws_default_route_table" "Lab_1_RT" {
-  default_route_table_id =  aws_vpc.Lab_1_VPC.default_route_table_id
+# Specify the default SG, regardless if the VPC creates it on default
+resource "aws_default_security_group" "Default_VPC_A_SG" {
+  vpc_id = aws_vpc.VPC_A.id
+
+  ingress {
+    protocol  = -1
+    from_port = 0
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "name" = "Default_VPC_A_SG"
+  }
+}
+
+# Create the Internet Gateway
+resource "aws_internet_gateway" "VPC_A_IGW" {
+  vpc_id = aws_vpc.VPC_A.id
+
+  tags = {
+    name = "VPC_A_IGW"
+  }
+}
+
+# Create the default route table
+resource "aws_default_route_table" "VPC_A_Default_RT" {
+  default_route_table_id = aws_vpc.VPC_A.default_route_table_id
 
   route {
-    cidr_block = var.Internet_IP_for_Route_a_string
-    gateway_id = aws_internet_gateway.Lab_1_IGW.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.VPC_A_IGW.id
   }
 
   tags = {
-    name           = "Lab_1_RT"
+    name = "VPC_A_Default_RT"
   }
 }
 
-resource "aws_instance" "AWS_Linux" {
-  ami                    = var.aws_instance_AWS_Linux_attributes.ami
-  instance_type          = var.aws_instance_AWS_Linux_attributes.instance_type
-  tenancy                = var.aws_instance_AWS_Linux_attributes.tenancy
-  availability_zone      = var.availability_zone
+# Create AWS instance
+resource "aws_instance" "VPC_A_Subnet_A_AWS_Linux" {
+  ami               = "ami-08cd358d745620807"
+  instance_type     = "t2.micro"
+  tenancy           = "default"
+  availability_zone = "eu-west-2a"
+  subnet_id = aws_subnet.VPC_A_Subnet_A.id
 
   tags = {
-    name           = "Lab_1_AWS_Linux"
+    name = "VPC_A_Subnet_A_AWS_Linux"
   }
 }
 
-resource "aws_eip" "Lab_1_EIP" {
-  instance = aws_instance.AWS_Linux.id
+# Create an Elastic IP
+resource "aws_eip" "VPC_A_EIP" {
+  instance = aws_instance.VPC_A_Subnet_A_AWS_Linux.id
   vpc      = true
   tags = {
-    name           = "Lab_1_EIP"
+    name = "VPC_A_EIP"
   }
 }
 
-resource "aws_eip_association" "Lab_1_EIP-Association" {
-  instance_id   = aws_instance.AWS_Linux.id
-  allocation_id = aws_eip.Lab_1_EIP.id
+# Associate the Elastic IP to the instance
+resource "aws_eip_association" "VPC_A_EIP-Association" {
+  instance_id   = aws_instance.VPC_A_Subnet_A_AWS_Linux.id
+  allocation_id = aws_eip.VPC_A_EIP.id
 }
