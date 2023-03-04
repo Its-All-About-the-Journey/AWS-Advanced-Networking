71d6bc601626ab86237c922dc96234906d5bd38f
Charles Uneze <charlesniklaus@gmail.com>
2023-03-04 14:38:30 +0100

    Update main.tf

--- Lab 2/main.tf
+++ Lab 2/main.tf
@@ -1,207 +1,220 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = var.region
-  #shared_credentials_files = "C:\\Users\\Admin\\.aws\\credentials"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  region = "eu-west-2"
+  access_key = ""
+  secret_key = ""
 }
 
-resource "aws_vpc" "Lab_2_VPC" {
-  cidr_block       = var.aws_vpc_Lab_2_VPC
+resource "aws_vpc" "VPC_A" {
+  cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "Lab_2_VPC"
+    Name = "VPC_A"
   }
 }
 
-resource "aws_internet_gateway" "Lab_2_IGW" {
-  vpc_id = aws_vpc.Lab_2_VPC.id
+resource "aws_internet_gateway" "VPC_A_IGW" {
+  vpc_id = aws_vpc.VPC_A.id
 
   tags = {
-    Name = "Labs_2_IGW"
+    Name = "VPC_A_IGW"
   }
 }
 
-resource "aws_subnet" "Public_A" {
-  vpc_id                  = aws_vpc.Lab_2_VPC.id
-  cidr_block              = var.aws_subnet_Public_A_attributes.cidr_block
-  availability_zone       = var.aws_subnet_Public_A_attributes.availability_zone
+resource "aws_subnet" "VPC_A_Public_Subnet_A" {
+  vpc_id                  = aws_vpc.VPC_A.id
+  cidr_block              = "10.0.20.0/24"
+  availability_zone       = "eu-west-2a"
   map_public_ip_on_launch = "true"
 
   tags = {
-    Name = "Public_A"
+    Name = "VPC_A_Public_Subnet_A"
   }
 }
 
-resource "aws_route_table" "Public_A_RT" {
-  vpc_id = aws_vpc.Lab_2_VPC.id
+resource "aws_route_table" "VPC_A_Public_RT_A" {
+  vpc_id = aws_vpc.VPC_A.id
 
   route {
-    cidr_block = var.Internet_IP_for_Route_a_string
-    gateway_id = aws_internet_gateway.Lab_2_IGW.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.VPC_A_IGW.id
   }
 
   tags = {
-    Name = "Public_A_RT"
+    Name = "VPC_A_Public_RT_A"
   }
 }
 
-resource "aws_route_table_association" "Public_A_RT_Association_A" {
-  subnet_id      = aws_subnet.Public_A.id
-  route_table_id = aws_route_table.Public_A_RT.id
+resource "aws_route_table_association" "VPC_A_Public_RT_A_Association_A" {
+  subnet_id      = aws_subnet.VPC_A_Public_Subnet_A.id
+  route_table_id = aws_route_table.VPC_A_Public_RT_A.id
 }
 
 resource "aws_instance" "Bastion" {
-  ami               = var.aws_instance_attributes.ami
-  instance_type     = var.aws_instance_attributes.instance_type
-  availability_zone = var.aws_instance_attributes.availability_zone
-  key_name          = var.aws_instance_attributes.key_name
-  tenancy           = var.aws_instance_attributes.tenancy
-  subnet_id         = aws_subnet.Public_A.id
-  security_groups   = ["${aws_security_group.SG_bastion.id}"]
+  ami               = "ami-08cd358d745620807"
+  instance_type     = "t2.micro"
+  availability_zone = "eu-west-2a"
+  key_name          = ""
+  tenancy           = "default"
+  subnet_id         = aws_subnet.VPC_A_Public_Subnet_A.id
+  security_groups   = ["${aws_security_group.SG_Bastion.id}"]
+
+  tags = {
+    "name" = "Bastion"
+  }
 }
 
-resource "aws_security_group" "SG_bastion" {
-  vpc_id      = aws_vpc.Lab_2_VPC.id
+resource "aws_security_group" "SG_Bastion" {
+  vpc_id      = aws_vpc.VPC_A.id
   description = " SG for bastion host. SSH access only"
 
   ingress {
-    from_port   = var.SSH_attributes.from_port
-    to_port     = var.SSH_attributes.to_port
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = var.Internet_IP_for_SG_a_list
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  #egress {
-  #  from_port   = 22
-  #  to_port     = 22
-  #  protocol    = "tcp"
-  #  cidr_blocks = ["0.0.0.0/0"]
-  #}
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "name" = "SG_Bastion"
+  }
+
 }
 
-resource "aws_subnet" "Private_A" {
-  vpc_id                  = aws_vpc.Lab_2_VPC.id
-  cidr_block              = var.aws_subnet_Private_A_attributes.cidr_block
-  availability_zone       = var.aws_subnet_Private_A_attributes.availability_zone
+resource "aws_subnet" "VPC_A_Private_Subnet_A" {
+  vpc_id                  = aws_vpc.VPC_A.id
+  cidr_block              = "10.0.10.0/24"
+  availability_zone       = "eu-west-2a"
   map_public_ip_on_launch = "false"
 
   tags = {
-    Name = "Public_A"
+    Name = "VPC_A_Private_Subnet_A"
   }
 }
 
-resource "aws_route_table" "Private_Route_Table" {
-  vpc_id = aws_vpc.Lab_2_VPC.id
+resource "aws_route_table" "VPC_A_Private_RT_A" {
+  vpc_id = aws_vpc.VPC_A.id
 
   route {
-    cidr_block = var.Internet_IP_for_Route_a_string
-    #gateway_id = aws_internet_gateway.Lab_2_IGW.id
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_nat_gateway.NAT_GW.id
   }
+
+  tags = {
+    Name = "VPC_A_Private_RT_A"
+  }
 }
 
-resource "aws_route_table_association" "Private_Route_Table_Association" {
-  subnet_id      = aws_subnet.Private_A.id
-  route_table_id = aws_route_table.Private_Route_Table.id
+resource "aws_route_table_association" "VPC_A_Private_RT_A_Association" {
+  subnet_id      = aws_subnet.VPC_A_Private_Subnet_A.id
+  route_table_id = aws_route_table.VPC_A_Private_RT_A.id
 }
 
-resource "aws_network_acl" "Private_NACL" {
-  vpc_id = aws_vpc.Lab_2_VPC.id
+resource "aws_default_network_acl" "Default_VPC_A_NACL" {
+  default_network_acl_id = aws_vpc.VPC_A.default_network_acl_id
 
   ingress {
-    protocol   = "tcp"
+    protocol   = -1
     rule_no    = 100
     action     = "allow"
-    cidr_block = aws_subnet.Public_A.cidr_block
-    from_port  = var.SSH_attributes.to_port
-    to_port    = var.SSH_attributes.to_port
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
   }
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    "name" = "Default_VPC_A_NACL"
+  }
+}
+
+resource "aws_network_acl" "VPC_A_Private_NACL_A" {
+vpc_id = aws_vpc.VPC_A.id
+subnet_ids = [aws_subnet.VPC_A_Private_Subnet_A.id]
 
   ingress {
-    protocol   = "tcp"
-    rule_no    = 200
-    action     = "allow"
-    cidr_block = var.Internet_IP_for_Route_a_string
-    from_port  = var.Euphemeral_ports_attributes.from_port
-    to_port    = var.Euphemeral_ports_attributes.to_port
-  }
-
-  egress {
-    protocol   = "tcp"
+    protocol   = -1
     rule_no    = 100
     action     = "allow"
-    cidr_block = var.Internet_IP_for_Route_a_string
-    from_port  = var.HTTP_attributes.from_port
-    to_port    = var.HTTP_attributes.to_port
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
   }
 
   egress {
-    protocol   = "tcp"
-    rule_no    = 200
+    protocol   = -1
+    rule_no    = 100
     action     = "allow"
-    cidr_block = var.Internet_IP_for_Route_a_string
-    from_port  = var.HTTP_attributes.from_port
-    to_port    = var.HTTP_attributes.to_port
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
   }
 
-  egress {
-    protocol   = "tcp"
-    rule_no    = 300
-    action     = "allow"
-    cidr_block = aws_subnet.Public_A.cidr_block
-    from_port  = var.Linux_ports_attributes.from_port
-    to_port    = var.Linux_ports_attributes.to_port
+  tags = {
+    "name" = "VPC_A_Private_NACL_A"
   }
 }
 
-resource "aws_network_acl_association" "Private_NACL_Association" {
-  network_acl_id = aws_network_acl.Private_NACL.id
-  subnet_id      = aws_subnet.Private_A.id
+resource "aws_network_acl_association" "VPC_A_Private_NACL_A_Association" {
+  network_acl_id = aws_network_acl.VPC_A_Private_NACL_A.id
+  subnet_id      = aws_subnet.VPC_A_Private_Subnet_A.id
 }
 
-resource "aws_instance" "Private_Instance" {
-  ami               = var.aws_instance_attributes.ami
-  instance_type     = var.aws_instance_attributes.instance_type
-  availability_zone = var.aws_instance_attributes.availability_zone
-  key_name          = var.aws_instance_attributes.key_name
-  tenancy           = var.aws_instance_attributes.tenancy
-  subnet_id         = aws_subnet.Private_A.id
+resource "aws_instance" "VPC_A_Private_Subnet_A_Instance_A" {
+  ami               = "ami-08cd358d745620807"
+  instance_type     = "t2.micro"
+  availability_zone = "eu-west-2a"
+  key_name          = ""
+  tenancy           = "default"
+  subnet_id         = aws_subnet.VPC_A_Private_Subnet_A.id
   security_groups   = ["${aws_security_group.SG_Private.id}"]
+
+  tags = {
+    "name" = "VPC_A_Private_Subnet_A_Instance_A"
+  }
 }
 
 resource "aws_security_group" "SG_Private" {
   name        = "SG_Private"
   description = "Security group for private subnet instances."
-  vpc_id      = aws_vpc.Lab_2_VPC.id
+  vpc_id      = aws_vpc.VPC_A.id
 
   ingress {
     description     = "Accept SSH inbound requests from Bastion host only."
-    from_port       = var.SSH_attributes.from_port
-    to_port         = var.SSH_attributes.to_port
+    from_port       = 22
+    to_port         = 22
     protocol        = "tcp"
-    security_groups = [aws_security_group.SG_bastion.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "Accept HTTPS inbound requests from Public VPC CIDR."
-    from_port   = var.HTTP_attributes.from_port
-    to_port     = var.HTTP_attributes.to_port
-    protocol    = "tcp"
-    cidr_blocks = ["${aws_vpc.Lab_2_VPC.cidr_block}"]
+  egress {
+    description = "Outgoing ICMP"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "name" = "SG_Private"
   }
 }
-
-resource "aws_security_group_rule" "To_Bastion_SG_allow_SSH_from_SG_Private_Only" {
-  type                     = "egress"
-  from_port                = var.SSH_attributes.from_port
-  to_port                  = var.SSH_attributes.to_port
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.SG_Private.id
-  security_group_id        = aws_security_group.SG_bastion.id
-}
-
 
 
 resource "aws_eip" "EIP_for_NAT_GW" {
@@ -209,8 +222,8 @@ resource "aws_eip" "EIP_for_NAT_GW" {
 }
 
 resource "aws_nat_gateway" "NAT_GW" {
-  subnet_id         = aws_subnet.Public_A.id
-  connectivity_type = var.aws_nat_gateway_NAT_GW_attributes
+  subnet_id         = aws_subnet.VPC_A_Public_Subnet_A.id
+  connectivity_type = "public"
   allocation_id     = aws_eip.EIP_for_NAT_GW.id
 
   tags = {
