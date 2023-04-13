c0578c5ce419d847bfbab0a9f2ff4a554f6c9199
Charles Uneze <charlesniklaus@gmail.com>
2023-04-13 12:26:05 +0100

    Update output.tf

--- Lab 4/output.tf
+++ Lab 4/output.tf
@@ -5,3 +5,8 @@ output "VPC_A_Public_Instance" {
 output "VPC_A_Private_Instance" {
   value = aws_instance.VPC_A_Private_Instance.private_ip
 }
+
+# Current S3 Object URL format
+output "object_s3_url" {
+  value = "https://${aws_s3_bucket.My_bucket.bucket}.s3.${var.aws_region}.amazonaws.com/${aws_s3_object.object.key}"
+}
