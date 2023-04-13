bd1fcadb7a1a240dfcd817d4eec2481e49abe119
Charles Uneze <charlesniklaus@gmail.com>
2023-04-13 08:33:09 +0100

    Update Networking.tf

--- Lab 4/Networking.tf
+++ Lab 4/Networking.tf
@@ -46,32 +46,6 @@ resource "aws_default_route_table" "Default_VPC_A_RT" {
   }
 }
 
-resource "aws_default_network_acl" "Default_VPC_A_NACL" {
-  default_network_acl_id = aws_vpc.VPC_A.default_network_acl_id
-
-  ingress {
-    protocol   = -1
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  egress {
-    protocol   = -1
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  tags = {
-    "name" = "Default_VPC_A_NACL"
-  }
-}
-
 resource "aws_default_security_group" "Default_VPC_A_SG" {
   vpc_id = aws_vpc.VPC_A.id
 
