eda201e537e061ee6315986d5f60811615b32d20
Charles Uneze <charlesniklaus@gmail.com>
2023-04-13 10:20:19 +0100

    Update output.tf

--- Lab 5/output.tf
+++ Lab 5/output.tf
@@ -2,10 +2,16 @@ output "consumer_vpc_public_instance_public_ip" {
     value = aws_instance.Consumer_VPC_Public_Instance.public_ip
 }
 
+# outputs the vpc-endpoint interface ID
 output "vpce_interface_ids" {
   value = join(",", aws_vpc_endpoint.vpce.network_interface_ids)
 }
 
+# copy the interface ID and uncomment the code below.
+# Add the ID into the data block. Then do a 
+# “terraform refresh” to get the private IP address
+# of the vpc-endpoint interface so you can SSH into it from
+# the public instance.
 /*
 # --------------------------------------------------------------
 data "aws_network_interface" "ENI_0" {
