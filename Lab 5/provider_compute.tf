db1f10911695dc711517eb65b2ff0c151e044789
Charles Uneze <charlesniklaus@gmail.com>
2023-04-13 08:21:28 +0100

    Update provider_compute.tf

--- Lab 5/provider_compute.tf
+++ Lab 5/provider_compute.tf
@@ -3,14 +3,10 @@ resource "aws_instance" "Provider_VPC_Private_Instance" {
   instance_type               = "t2.micro"
   tenancy                     = "default"
   availability_zone           = "us-west-2a"
-  key_name                    = "CharlesUneze"
+  key_name                    = ""
   subnet_id                   = aws_subnet.private_subnet_for_Provider_VPC_AZ_2A.id
   security_groups             = ["${aws_security_group.Provider_VPC_SG.id}"]
-  user_data                   = <<EOF
-    # !/bin/bash
-    sudo yum install -y httpd
-    sudo service httpd start
-  EOF
+  
 
   tags = {
     "name" = "Provider_VPC_Private_Instance"
