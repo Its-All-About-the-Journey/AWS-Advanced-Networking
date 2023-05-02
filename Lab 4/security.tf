12fd86f34dac9ee17d9f1b054a3ca545a8eeac53
Charles Uneze <charlesniklaus@gmail.com>
2023-05-02 22:39:20 +0100

    Delete security.tf

--- Lab 4/security.tf
+++
@@ -1,6 +0,0 @@
-# Grants user full access to S3 so it can modify ACL rules.
-resource "aws_iam_policy_attachment" "s3_full_access" {
-  name       = "s3_full_access_attachment"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-  users      = ["iamadmin"]
-}
