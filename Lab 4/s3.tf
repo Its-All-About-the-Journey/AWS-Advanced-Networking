1704ffe160923512464d41d04eb89029633f944c
Charles Uneze <charlesniklaus@gmail.com>
2023-05-02 22:39:59 +0100

    Update s3.tf

--- Lab 4/s3.tf
+++ Lab 4/s3.tf
@@ -5,18 +5,44 @@ resource "aws_s3_bucket" "My_bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "my_bucket_acl" {
-  bucket = aws_s3_bucket.My_bucket.id
-  acl    = "public-read"
-}
-
 resource "aws_s3_object" "object" {
-  bucket = "my-test-bucket-for-vpc-endpoints"
+  bucket = aws_s3_bucket.My_bucket.bucket
   key    = "curl.txt"
   source = "curl.txt"
-  acl    = "public-read"
 
   etag = filemd5("curl.txt")
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.My_bucket.bucket
 
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.My_bucket.bucket
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression's result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "MYBUCKETPOLICY"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = [
+                "s3:GetObject"
+            ]
+        Resource = [
+          aws_s3_bucket.My_bucket.arn,
+          "${aws_s3_bucket.My_bucket.arn}/*",
+        ]
+      },
+    ]
+  })
+}
