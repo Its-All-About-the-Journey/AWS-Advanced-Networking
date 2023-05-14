13d197a6b76c3ed6241e3a49b6a9ad506cbea0c1
Charles Uneze <charlesniklaus@gmail.com>
2023-05-14 19:16:14 +0100

    Create variable.tf

---
+++ Lab 8a/variable.tf
@@ -0,0 +1,14 @@
+variable "website" {
+  type    = string
+  default = "Origin 1"
+}
+
+variable "website_2" {
+  type    = string
+  default = "Origin 2"
+}
+
+variable "hosted_zone_id" {
+  type    = string
+  default = ""
+}
