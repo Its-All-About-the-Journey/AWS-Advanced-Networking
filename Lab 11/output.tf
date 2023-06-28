e7e3859597fd01c0c28577a79092a63afcdb1fb1
Charles Uneze <charlesniklaus@gmail.com>
2023-06-28 10:59:07 +0100

    Create output.tf

---
+++ Lab 11/output.tf
@@ -0,0 +1,46 @@
+# The public IP addresses in AZ A
+output "Public_Instances_1" {
+  value = aws_instance.Public_Instances_Fleet_1[0].public_ip
+  description = "Marketing Department"
+}
+
+output "Public_Instances_2" {
+  value = aws_instance.Public_Instances_Fleet_1[1].public_ip
+  description = "Sales Department"
+}
+
+# The public IP addresses in AZ B
+output "Public_Instances_3" {
+  value = aws_instance.Public_Instances_Fleet_2[0].public_ip
+  description = "Marketing Department"
+}
+
+output "Public_Instances_4" {
+  value = aws_instance.Public_Instances_Fleet_2[1].public_ip
+  description = "Sales Department"
+}
+
+output "Public_Instances_1_subnet" {
+  value = aws_instance.Public_Instances_Fleet_1[0].subnet_id
+}
+
+output "Public_Instances_2_subnet" {
+  value = aws_instance.Public_Instances_Fleet_1[1].subnet_id
+}
+
+output "Public_Instances_3_subnet" {
+  value = aws_instance.Public_Instances_Fleet_2[0].subnet_id
+}
+
+output "Public_Instances_4_subnet" {
+  value = aws_instance.Public_Instances_Fleet_2[1].subnet_id
+}
+
+# DNS for Marketing and sales
+output "My_App_LB_DNS_Marketing" {
+  value = "${aws_lb.My_App_LB.dns_name}/marketing.html"
+}
+
+output "My_App_LB_DNS_Sales" {
+  value = "${aws_lb.My_App_LB.dns_name}/sales.html"
+}
