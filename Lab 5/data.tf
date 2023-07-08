b19bb45e3d7c6148856403b27feb3649c903ba29
Charles Uneze <charlesniklaus@gmail.com>
2023-07-08 17:30:32 +0100

    Update data.tf

--- Lab 5/data.tf
+++ Lab 5/data.tf
@@ -1,5 +1,5 @@
 # Reads the data of the VPC Endpoint, specifically the interface ID, 
 
 data "aws_network_interface" "ENI_0" {
-  id = local.key
+  id = join(",", aws_vpc_endpoint.vpce.network_interface_ids)
 }
