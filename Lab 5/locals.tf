9065a0e707f840978713a36eb645a3bd75bd48fd
Charles Uneze <charlesniklaus@gmail.com>
2023-07-08 17:30:10 +0100

    Delete locals.tf

--- Lab 5/locals.tf
+++
@@ -1,6 +0,0 @@
-# Specifies the interface ID of the VPC endpoint as a string so it can be used 
-# anywhere in the code.
-
-locals {
-  key = join(",", aws_vpc_endpoint.vpce.network_interface_ids)
-}
