64b7a6f4a98ed13f1468c09524fa87c532ae73c4
Charles Uneze <charlesniklaus@gmail.com>
2023-09-13 08:49:13 +0100

    Add files via upload

---
+++ Lab 17/marketing_a.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo chmod 777 /var/www/html
+cd /var/www/html
+echo "<!DOCTYPE html><html><head><title>Marketing Department</title></head><body><h1>Marketing Department</h1><p>This is the Marketing department A web page.</p></body></html>" > index.html
---
+++ Lab 17/sales_b.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo chmod 777 /var/www/html
+cd /var/www/html
+echo "<!DOCTYPE html><html><head><title>Sales Department</title></head><body><h1>Sales Department</h1><p>This is the sales department B web page.</p></body></html>" > index.html
