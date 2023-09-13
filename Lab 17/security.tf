b3cdc6be5408a61b0b1c780545dfd3ebc6dc831c
Charles Uneze <charlesniklaus@gmail.com>
2023-09-13 08:50:21 +0100

    Create security.tf

---
+++ Lab 17/security.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "My_VPC_SG" {
+  name        = "My_VPC_SG"
+  description = "Allow SSH, HTTP and HTTPS and ICMP inbound traffic"
+  vpc_id      = aws_vpc.My_VPC.id
+
+  ingress {
+    description = "SSH into VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP into VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS into VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "ICMP into VPC"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "ICMP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "My_VPC_SG"
+  }
+}
+
