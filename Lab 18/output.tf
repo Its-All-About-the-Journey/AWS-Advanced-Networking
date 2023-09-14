b887d54c9c5ca303be39abf4a228542d01ffe00d
Charles Uneze <charlesniklaus@gmail.com>
2023-09-14 20:47:45 +0100

    Update output.tf

--- Lab 18/output.tf
+++ Lab 18/output.tf
@@ -14,9 +14,5 @@ output "Security1" {
     value = aws_instance.Security1.public_ip
 }
 
-output "Security2" {
-    value = aws_instance.Security2.public_ip
-}
-
 # SSH into instance
 # aws ec2-instance-connect ssh --instance-id i-0e094c49ff7b697cd
