7b34e82985d2c62581f23832931128e3ade73e38
Charles Uneze <charlesniklaus@gmail.com>
2023-09-14 20:39:25 +0100

    Create output.tf

---
+++ Lab 18/output.tf
@@ -0,0 +1,14 @@
+output "My_App_LB_DNS" {
+  value = "${aws_lb.My_GWLB.dns_name}"
+}
+
+output "APP1" {
+    value = aws_instance.APP1.id
+}
+
+output "APP2" {
+    value = aws_instance.APP2.id
+}
+
+# SSH into instance
+# aws ec2-instance-connect ssh --instance-id i-0e094c49ff7b697cd
