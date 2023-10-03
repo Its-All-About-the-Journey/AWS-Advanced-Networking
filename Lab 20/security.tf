b543d79e83c06c3147ff807a81089d15bd97925f
Charles Uneze <charlesniklaus@gmail.com>
2023-10-03 10:37:11 +0100

    Create security.tf

---
+++ Lab 20/security.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "SG_A" {
+  vpc_id      = aws_vpc.VPC_A.id
+  description = "VPC A"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "SG_B" {
+  vpc_id      = aws_vpc.VPC_B.id
+  description = "VPC B"
+
+ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
